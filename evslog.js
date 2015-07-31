c4f3c102dea490f6c7dabb388f84a643b4f1ccb9
Evelyn Eastmond <evhan55@gmail.com>
2015-07-30 20:03:32 -0400

    Adding Evelyn's week log of experiments.

---
+++ evslog.js
@@ -0,0 +1,189 @@
+/* TO EVAL CONTENTS: Type evalEditor() in the console. */
+
+// TEST CODE
+/*
+function test() {
+ var x = 1;
+ var y = 0;
+ var w = 10;
+ console.log('test is run x: ' + x);
+ console.log('test is run y: ' + y);
+ console.log('test is run z: ' + w);
+}
+
+// run
+test();
+*/
+
+// numbers...
+// meaningful...
+// ?? brackets
+// functions } } } 
+// arrays?
+// figures?
+
+// systems have to be able to cope with errors
+// lots of programming assumes input is perfect
+// think about problems when calling data into question
+// solve the problem of... dealing with...
+
+// solve the problem of...
+// experience 
+
+// house of cards
+// malleable
+// conversation with material
+// process/experience/control/letting-go
+// the program isn't something to dominate
+
+/*
+var actions = ["Read", "Google", "Forget", "Write", "Question"];
+var objects = ["Nesbit", "Sabi", "Focillon", "Herbert", "Phaidon"];
+var frequency = ["Again", "Never", "Always", "First", "Second", "Street Number"];
+var result = "";
+
+result = actions[Math.floor(Math.random()*actions.length)] + " " +
+  objects[Math.floor(Math.random()*objects.length)] + " " +
+  frequency[Math.floor(Math.random()*frequency.length)];
+console.log(result);
+*/
+
+// document.getElementById('dom').innerHTML = 'hiiii';
+
+// I got what I wanted :(
+
+/*var num = words.length;
+var times = 0;
+for(var i = 0; i < num; i++) {
+  for(var j = 0; j < times; j++) {
+  	console.log(words[j]);
+  }
+}*/
+
+/*
+var a = document.createElement('a');
+a.innerHTML = 'link';
+a.setAttribute('href', 'http://www.google.com');
+document.getElementById('dom').appendChild(a);
+*/
+/*
+// ACCESS DOM
+var mydom = document.getElementById('dom');
+
+// CLEAR DOM
+mydom.innerHTML = '';
+document.body.setAttribute('style', 'font-family: sans-serif;');
+
+// ADD HEADER
+var h1 = document.createElement('h1');
+var borderWidth = ---11.01;
+var borderStyle = 'border-left: ' + borderWidth + 'px solid red;';
+h1.innerHTML = 'My Research Goals';
+h1.setAttribute('style', borderStyle);
+//a.setAttribute('href', 'http://www.google.com');
+mydom.appendChild(h1);
+
+// ADD INPUT PROMPT
+var h3 = document.createElement('h3');
+var borderWidth = 0.99;
+var borderStyle = 'border-left: ' + borderWidth + 'px solid blue;';
+h3.innerHTML = 'Goal:';
+h3.setAttribute('style', borderStyle);
+//a.setAttribute('href', 'http://www.google.com');
+mydom.appendChild(h3);
+
+// ADD INPUT PROMPT SUBHEADER
+var h4 = document.createElement('label');
+var gray = -----1.0009900000000016;
+var h4Style = 'color: rgb(' + gray + ',' + gray + ',' + gray + '); font-style: italic;';
+h4.innerHTML = 'Type a goal or ';
+h4.setAttribute('style', h4Style);
+mydom.appendChild(h4);
+
+// ADD GENERATE BUTTON
+var b = document.createElement('button');
+b.innerHTML = 'generate a goal';
+mydom.appendChild(b);
+
+// ADD INPUT FIELD
+var i = document.createElement('input');
+var inputWidth = 29787889;
+var inputStyle = 'width: ' + inputWidth + 'px';
+i.setAttribute('type', 'text');
+i.setAttribute('style', inputStyle);
+mydom.appendChild(document.createElement('br'));
+mydom.appendChild(document.createElement('br'));
+mydom.appendChild(i);
+
+// ADD INPUT BUTTON
+var b2 = document.createElement('button');
+b2.innerHTML = 'submit';
+mydom.appendChild(b2);
+
+// ADD TEXTAREA
+var t = document.createElement('textarea');
+var t_w = -----181.20000000000005;
+var t_h = 0.0103;
+t.setAttribute('style', 'width: ' + t_w + 'px; height: ' + t_h + 'px;');
+mydom.appendChild(document.createElement('br'));
+mydom.appendChild(document.createElement('br'));
+mydom.appendChild(t);
+*/
+// **************************************************
+// MUTATIONS:
+// sometimes a number is changed
+//    do you worry about floating points?
+//    do you worry about significance?
+// sometimes a string is changed
+// sometimes a length is changed?
+// sometimes an if/else is reversed?
+// sometimes a plane flies overhead
+// sometimes a line of code is removed?
+
+// QUESTIONS:
+// How to make things not fragile?
+// What is the notion of meaning?
+// What is significant?
+// Should changes be permanent or remember themselves?
+// 
+// LESSONS:
+//  30% seems too often!
+//  Changed to 20% and 10%, still too much, trying 1%
+//  1/1000 seemed too rare maybe, I got bored
+//  Trying 1/500
+// HAD TO GO BACK TO 30% TO SEE ANYTHING
+// JavaScript carries so much baggage
+// Level of logic/abstraction that is interesting
+// Ambient calculus
+// Process calculus
+// 
+
+// Don't usually set out to program something that has certain programming constructs.
+// If I program like that it feels forced.
+
+// bbbbbbbbbbbbbbbbbbbbbbbbbbbbddddddddddbbbbbbbbb
+// aaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaabbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaa
+// nhnhnhnhnhnhnhn
+
+var x = --44;
+if(x < 9.96) {                                                                    
+  console.log("whatever");
+} else {                                                                  
+  console.log('hehe');
+}
+
+if(940.06.006) {                                                                  
+  console.log('hehe');
+} else {                                                            
+  console.log("whatever");
+  console.log("test");
+}
+
+if(-10> 10) {                                                      
+  console.log("whatever");
+onsole.log("pat");} else {                                          
+  console.log('hehe');
+  c
+}
+
+// sssssssss
